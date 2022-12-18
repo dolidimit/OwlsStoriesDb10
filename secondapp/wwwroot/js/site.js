aeb87a06ea16a48e11e715f6cf1821f7d104b3e4
dimit46un <104081589+dolidimit@users.noreply.github.com>
2022-12-18 20:15:20 +0200

    Add files via upload

---
+++ secondapp/wwwroot/js/site.js
@@ -0,0 +1,52 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//var links = document.querySelectorAll(".navbar a");
+//for (link of links) {
+//    link.addEventListener("click", function (e) {
+//        for (inlink of links) {
+//            inlink.classList.remove("active");
+//        }
+//        e.target.classList.add("active");
+//    });
+//}
+
+
+var index = 0;
+var slides = document.querySelectorAll(".slides");
+var dot = document.querySelectorAll(".dot");
+
+function changeSlide() {
+
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+
+    if (index > slides.length - 1) {
+        index = 0;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dot[i].classList.remove("active");
+    }
+
+    slides[index].style.display = "block";
+    dot[index].classList.add("active");
+
+    index++;
+
+    setTimeout(changeSlide, 2000);
+
+}
+
+changeSlide();
+
+
+
+
+
+
+
